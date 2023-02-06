2ffe4c1cc0768289136b7d20f46f3ace2efdc4e1
gtraiano <gtraiano@gmail.com>
2023-02-06 12:11:25 +0200

    added unit test for Modal component

---
+++ src/tests/unit/components/Modal.test.tsx
@@ -0,0 +1,31 @@
+import { render, fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
+import Modal from '../../../components/Modal'
+import style from '../../../components/Modal/style.module.css'
+
+describe('Modal', () => {
+    test('empty content renders no content', async () => {
+        const { container } = render(<Modal toggleModal={() => {}} />);
+        expect(container.getElementsByClassName(`${style['modal-content']}`)[0].childElementCount).toBe(0);
+    });
+
+    test('provided content renders properly', () => {
+        const { container } = render(<Modal toggleModal={() => {}} ><div id="provided-content"><h1>Content</h1></div></Modal>);
+        expect(container.querySelector('#provided-content')).toBeDefined();
+        expect(container.querySelector(`.${style['modal-content']} > *`)?.id).toBe('provided-content');
+    });
+
+    test('click on close button executes toggleModal callback', () => {
+        let show = true;
+        const toggleModal = () => { show = !show; };
+        const spy = vi.fn().mockImplementation(toggleModal);
+        const modal = <Modal toggleModal={spy} />;
+        
+        let { container, rerender } = render(show ? modal : <></>);
+        fireEvent.click(container.querySelector('#close-button') as Element);
+        rerender(show ? modal : <></>);
+        
+        expect(container.childElementCount).toBe(0);
+        expect(spy).toHaveBeenCalledTimes(1);
+    })
+});
